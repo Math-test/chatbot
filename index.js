78d849f1ad56728c3e3b0b477b3fb6e9e47e0013
rosette <irene153232@gmail.com>
2021-01-20 00:26:50 +0800

    恢復原狀

--- index.js
+++ index.js
@@ -8,34 +8,37 @@ let bot = linebot({
 });
 
 // 當有人傳送訊息給 Bot 時
-// bot.on('message', function (event) {
-//   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
-//   console.log(`${event.message.text}`);
-//   switch(event.message.text){
-//     case'hello':
-//     event.reply('你上線了')
-//     break
-//     case'問':
-//     event.reply('有啥問題')
-//     break
-//     case'壞人':
-//     event.reply('別生氣了')
-//     break
-//     case'開心':
-//     event.reply('恭喜你,希望你過得好')
-//     break
-//     default:
-//       event.reply('(放空中)')
-//       break
-//   }
-// });
+bot.on('message', function (event) {
+  // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
+  console.log(`${event.message.text}`);
+  switch(event.message.text){
+    case'hello':
+    event.reply('你上線了')
+    break
+    case'問':
+    event.reply('有啥問題')
+    break
+    case'壞人':
+    event.reply('別生氣了')
+    break
+    case'開心':
+    event.reply('恭喜你,希望你過得好')
+    break
+    default:
+      event.reply('(放空中)')
+      break
+  }
+});
 主動發送訊息
 setTimeout(function(){
     var userId = '124';
     var sendMsg = '學生到班囉';
     bot.push(userId,sendMsg);
     bot.on('message', function (event) {
-     console.log('send: '+sendMsg);
+      event.reply(sendMsg);
+      
+    });
+    console.log('send: '+sendMsg);
 },5000);
 // const line = require('@line/bot-sdk');
 
